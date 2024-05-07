94b20df74463b148f978ad1b348f06a30a544eeb
Ágoston Reguly <40354586+regulyagoston@users.noreply.github.com>
2024-05-07 16:38:07 +0200

    data german munged

--- R/data.R
+++ R/data.R
@@ -1,7 +1,9 @@
 #' German reunification on West Germany
 #'
+#' @docType data
+#' @name german_reunification
 #'
-#' @format ## `german_reunification`
+#' @format To be updated
 #' A data frame with:
 #' \describe{
 #'   \item{country}{Country name}
@@ -9,4 +11,12 @@
 #'   \item{year}{Year}
 #'   ...
 #' }
+#' @usage data(german_reunification)
+#'
+#' @examples
+#' \donttest{
+#' # Load German reunification long panel format
+#' data("german_reunification")
+#' }
+#'
 NULL
